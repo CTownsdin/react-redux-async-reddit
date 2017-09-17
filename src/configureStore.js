4d94be6689a4b0787e31649c656dd52435e9d629
Christian Townsdin <ctownsdin@expedia.org>
2017-09-17 11:58:52 -0700

    not using composeEnhancers

--- src/configureStore.js
+++ src/configureStore.js
@@ -6,7 +6,7 @@ import rootReducer from './reducers'
 const loggerMiddleware = createLogger()
 
 import { composeWithDevTools } from 'redux-devtools-extension';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
   return createStore(rootReducer, 
